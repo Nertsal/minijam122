6b9752cf4f8ec44863d8532e52f2065c678fcb77
Nertsal <sasha.kudasov04@gmail.com>
2022-12-23 12:43:23 +0300

    Level visualized

--- src/assets.rs
+++ src/assets.rs
@@ -1,4 +1,13 @@
 use super::*;
 
 #[derive(geng::Assets)]
-pub struct Assets {}
+pub struct Assets {
+    #[asset(load_with = "gltf_load::load_meshes(&geng, base_path.join(\"level.glb\"))")]
+    pub level: Vec<Mesh>,
+    pub shaders: Shaders,
+}
+
+#[derive(geng::Assets)]
+pub struct Shaders {
+    pub gltf: ugli::Program,
+}
--- src/game.rs
+++ src/game.rs
@@ -3,6 +3,7 @@ use super::*;
 pub struct Game {
     geng: Geng,
     assets: Rc<Assets>,
+    camera: Camera,
 }
 
 impl Game {
@@ -10,6 +11,13 @@ impl Game {
         Self {
             geng: geng.clone(),
             assets: assets.clone(),
+            camera: Camera {
+                fov: f32::PI / 3.0,
+                pos: vec3(0.0, 0.0, 1.0),
+                distance: 5.0,
+                rot_h: 0.0,
+                rot_v: f32::PI / 3.0,
+            },
         }
     }
 }
@@ -17,5 +25,30 @@ impl Game {
 impl geng::State for Game {
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
         ugli::clear(framebuffer, Some(Rgba::BLACK), Some(1.0), None);
+        let framebuffer_size = framebuffer.size().map(|x| x as f32);
+        for mesh in &self.assets.level {
+            ugli::draw(
+                framebuffer,
+                &self.assets.shaders.gltf,
+                ugli::DrawMode::Triangles,
+                &mesh.data,
+                (
+                    mesh.material.uniforms(),
+                    ugli::uniforms! {
+                        u_model_matrix: Mat4::identity(), // TODO
+                        u_eye_pos: self.camera.eye_pos(),
+                        u_light_dir: vec3(1.0, -2.0, 5.0),
+                        u_light_color: Rgba::WHITE,
+                        u_ambient_light_color: Rgba::WHITE,
+                        u_ambient_light_intensity: 0.1,
+                    },
+                    geng::camera3d_uniforms(&self.camera, framebuffer_size),
+                ),
+                ugli::DrawParameters {
+                    depth_func: Some(ugli::DepthFunc::Less),
+                    ..default()
+                },
+            );
+        }
     }
 }
